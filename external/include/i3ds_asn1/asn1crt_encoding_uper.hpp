1a342d8221e26a2ca0eaed305c9789c64f4826c7
Henrik Austad <henrik.austad@sintef.no>
2019-11-19 15:36:52 +0100

    Add output from -interfaces directly
    i3ds-interfaces.git since commit d41d9294d0e will generate a tar-archive
    with headers and a compiles .so for the ASN.1 encoder/decoder. This is
    unpacked locally to ease the transition away from generated/
    
    This unpacks the latest archive (i3ds_asn1-2020-01-d41d9294d0ef.tar) to
    external/

---
+++ external/include/i3ds_asn1/asn1crt_encoding_uper.hpp
@@ -0,0 +1,25 @@
+#ifndef i3ds_asn1_ASN1SCC_ASN1CRT_ENCODING_UPER_H_
+#define i3ds_asn1_ASN1SCC_ASN1CRT_ENCODING_UPER_H_
+
+#include <i3ds_asn1/asn1crt_encoding.hpp>
+namespace i3ds_asn1 {
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+void ObjectIdentifier_uper_encode(BitStream* pBitStrm, const Asn1ObjectIdentifier *pVal);
+flag ObjectIdentifier_uper_decode(BitStream* pBitStrm, Asn1ObjectIdentifier *pVal);
+void RelativeOID_uper_encode(BitStream* pBitStrm, const Asn1ObjectIdentifier *pVal);
+flag RelativeOID_uper_decode(BitStream* pBitStrm, Asn1ObjectIdentifier *pVal);
+
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+
+} // namespace i3ds_asn1
+#endif
