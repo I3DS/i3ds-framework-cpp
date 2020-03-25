28c5346de0ccb4b70d3d11b35b6f91ead0a2b632
Henrik Austad <henrik.austad@sintef.no>
2020-03-25 15:57:17 +0100

    update petamake to handle 2019.x sysroot
    The sysroot has changed slightly and the environment we source will
    populate variables used by cmake to set CMAKE_FIND_ROOT_PATH. When we
    export this from petamake, we end up with a double sysroot, causing
    build to fail.
    
    We solve this by bypassing this and setting BUILD_SYSROOT instead, and
    cmake will use this over FIND_ROOT_PATH whenever available.
    
    This has been tested with both 2019.1 and 2019.2

--- petamake.sh
+++ petamake.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set -e
-source /opt/petalinux/settings.sh
-source /opt/Xilinx/SDK/2019.1/settings64.sh
-BUILDPATH="petalinux_build"
+BSP_PATH="/opt/bsp"
+BSP_NAME="eross_v3"
+# BSP_NAME=eross_v4
 
-BSP_NAME=i3ds-bsp-v3
-BSP_ROOT=/opt/bsp/$BSP_NAME/build/tmp/sysroots/plnx_aarch64
+if [[ "${BSP_NAME}" == "eross_v3" ]]; then
+    source /opt/petalinux/2019.1/settings.sh
+elif [[ "${BSP_NAME}" == "eross_v4" ]]; then
+    source /opt/petalinux/2019.2/settings.sh
+else
+    echo "Unkonwn BSP"
+    exit 0
+fi
+
+source ${BSP_PATH}/$BSP_NAME/images/linux/sdk/environment-setup-aarch64-xilinx-linux
+BSP_ROOT=${BSP_PATH}/$BSP_NAME/images/linux/sdk/sysroots/aarch64-xilinx-linux
+
+BUILDPATH="petalinux_build"
 
 mkdir -p ${BUILDPATH}
 cd ${BUILDPATH}
-cmake .. -DCMAKE_C_COMPILER="aarch64-linux-gnu-gcc" \
-      -DCMAKE_CXX_COMPILER="aarch64-linux-gnu-c++" \
-      -DCMAKE_FIND_ROOT_PATH="$BSP_ROOT" \
+
+cmake .. -DCMAKE_C_COMPILER="aarch64-xilinx-linux-gcc" \
+      -DCMAKE_CXX_COMPILER="aarch64-xilinx-linux-g++" \
+      -DBUILD_SYSROOT="${BSP_ROOT}" \
       -DBUILD_EMBEDDED=ON \
       -DBUILD_DEBUG=OFF \
       -DBUILD_TOOLS=ON \
