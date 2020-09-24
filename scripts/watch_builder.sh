b39fdd903a01f4519f26c6d98cbabb60710239a3
Henrik Austad <henrik.austad@sintef.no>
2020-09-24 13:21:55 +0200

    scripts: add external/ in watch-builder, don't make with VERBOSE=1
    watch-builder is used during development to gather rapid feedback and
    will most likely use the latest version of i3ds-interfaces.

--- scripts/watch_builder.sh
+++ scripts/watch_builder.sh
@@ -8,7 +8,6 @@ BUILDPATH=$(grep "BUILDPATH=" scripts/hostmake.sh | cut -d '=' -f2|sed 's/\"//g'
 test -z ${NUM_CPUS} && NUM_CPUS=$(grep ^cpu\ MHz /proc/cpuinfo | wc -l)
 
 dirs=$(for f in $(find . -name "*.[c|h][pp]?" -o -name "CMakeLists.txt"); do dirname ${f}; done|grep -v _build|sort|uniq)
-dirs="${dirs} ./generated/i3ds/"
 
 if [[ -z ${dirs} ]]; then
     echo "Could not find any source-files, try to move to the root of the project!"
@@ -18,7 +17,7 @@ fi
 inotifywait -m ${dirs} -e modify -e create --exclude "\#" |
     # If buildpath isn't available, we have to run full compile anyway
     if [[ ! -d ${BUILDPATH} && ! -z ${action} ]]; then
-	echo "time ./scripts/hostmake.sh -c -t"
+	echo "time ./scripts/hostmake.sh -c -t -e external/"
     else
 	while read path action file; do
 	    echo "path: ${path}"
@@ -32,20 +31,20 @@ inotifywait -m ${dirs} -e modify -e create --exclude "\#" |
 			# workaround for libi3ds
 			test -e ${path}/Makefile || path=$(dirname ${path})
 			pushd ${BUILDPATH}/${path}
-			time make VERBOSE=1 -j${NUM_CPUS}
+			time make -j${NUM_CPUS}
 			popd
 	    		;;
 
 	    		*.h[pp]?)
 	    	        echo "[M] headers have changed, do a system-wide (fast) rebuild"
 			pushd ${BUILDPATH}
-			time make VERBOSE=1 -j${NUM_CPUS}
+			time make -j${NUM_CPUS}
 			popd
 
 	    		;;
 	    		CMakeLists.txt)
 	    		    echo "${path}/${file} updated, trigger full rebuild"
-	    		    time ./scripts/hostmake.sh -c
+	    		    time ./scripts/hostmake.sh -c -e external/
 	    		    ;;
 	    		*)
 	    		    # unknown file, ignore
@@ -57,16 +56,16 @@ inotifywait -m ${dirs} -e modify -e create --exclude "\#" |
 	    		*.c[pp]?)
 			test -e Makefile ${path} || path=$(dirname ${path})
 			pushd ${BUILDPATH}/${path}
-			time make VERBOSE=1 -j${NUM_CPUS}
+			time make  -j${NUM_CPUS}
 			popd
 	    		;;
 	    		*.h[pp]?)
 	    		echo "[C] new header created, trigger full rebuild"
-	    		time ./scripts/hostmake.sh -c -t all
+	    		time ./scripts/hostmake.sh -c -t all -e external/
 	    		;;
 	    		CMakeLists.txt)
 	    		    echo "[C] ${path}/${file} created, trigger full rebuild"
-	    		    time ./scripts/hostmake.sh -c
+	    		    time ./scripts/hostmake.sh -c -e external/
 	    		    ;;
 	    		*)
 	    		    # unknown, ignore
