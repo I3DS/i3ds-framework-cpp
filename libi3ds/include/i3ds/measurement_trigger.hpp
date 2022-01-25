1f5dab1f00ce670fa6e2ee601a22c1eac3e34cbe
Fredrik Bakkevig Haugli <fredrik.haugli@sintef.no>
2022-01-25 15:39:17 +0100

    Adds measurement trigger

---
+++ libi3ds/include/i3ds/measurement_trigger.hpp
@@ -0,0 +1,133 @@
+///////////////////////////////////////////////////////////////////////////\file
+///
+///   Copyright 2022 SINTEF AS
+///
+///   This Source Code Form is subject to the terms of the Mozilla
+///   Public License, v. 2.0. If a copy of the MPL was not distributed
+///   with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __I3DS_MEASUREMENT_FETCHER_HPP
+#define __I3DS_MEASUREMENT_FETCHER_HPP
+
+#include<boost/log/trivial.hpp>
+
+#ifndef BOOST_LOG_DYN_LINK
+#define BOOST_LOG_DYN_LINK
+#endif
+
+#include <boost/log/trivial.hpp>
+
+#include <i3ds/subscriber.hpp>
+#include <i3ds/sensor_client.hpp>
+
+#include <mutex>
+#include <stdexcept>
+
+namespace i3ds
+{
+
+////////////////////////////////////////////////////////////////////////////////
+/// Utility function for fetching single measurements instead of subscribing
+////////////////////////////////////////////////////////////////////////////////
+
+template<typename Topic>
+class MeasurementTrigger
+{
+public:
+
+  MeasurementTrigger(Context::Ptr context, i3ds_asn1::NodeID node, bool capture_measurement = false) :
+      capture_measurement_(capture_measurement),
+      subscriber_(context),
+      client_(context, node),
+      valid_measurement_(false),
+      waiting_(false),
+      started_by_trigger_(false)
+    {
+      subscriber_.Attach<Topic>(node, [this](typename Topic::Data& msg){handle_measurement(msg);});
+      subscriber_.Start();
+    };
+  virtual ~MeasurementTrigger()
+    {
+      subscriber_.Stop();
+    }
+
+  // Trigger a single measurement
+  void trigger()
+    {
+      std::lock_guard<std::mutex> guard(mutex);
+      BOOST_LOG_TRIVIAL(trace) << "[MeasurementTrigger] trigger()";
+      client_.load_status();
+      if (client_.is_standby())
+        {
+          BOOST_LOG_TRIVIAL(trace) << "[MeasurementTrigger] starting sensor";
+          client_.set_state(i3ds_asn1::StateCommand_start);
+          started_by_trigger_ = true;
+        }
+      else
+        {
+          started_by_trigger_ = false;
+        }
+      waiting_ = true;
+    }
+
+  // Will return the latest measurement sent by the sensor
+  // Throws logic_error if the no measurement is available
+  typename Topic::Data& fetch()
+    {
+      std::lock_guard<std::mutex> guard(mutex);
+      if (!valid_measurement_)
+        {
+           throw std::logic_error("No valid measurement to fetch");
+        }
+      return measurement_;
+    }
+  
+private:
+
+  void handle_measurement(typename Topic::Data& measurement)
+    {
+      std::lock_guard<std::mutex> guard(mutex);
+      BOOST_LOG_TRIVIAL(trace) << "[MeasurementTrigger] handle_measurement";
+      if (started_by_trigger_)
+        {
+          BOOST_LOG_TRIVIAL(trace) << "[MeasurementTrigger] stopping sensor";
+          client_.set_state(i3ds_asn1::StateCommand_stop);
+        }
+      if (capture_measurement_)
+        {
+          measurement_ = measurement;
+          valid_measurement_ = true;
+        }
+      waiting_ = false;
+    }
+
+  // Should the message body be kept for fetching
+  bool capture_measurement_;
+
+  // Subscriber for fetching measurements
+  Subscriber subscriber_;
+
+  // Client for starting the sensor
+  SensorClient client_;
+
+  // Last measurement received
+  typename Topic::Data measurement_;
+
+  // Is the measurement valid?
+  bool valid_measurement_;
+
+  // Is the fetcher waiting for a measurement?
+  bool waiting_;
+
+  // Did the trigger start the sensor?
+  bool started_by_trigger_;
+
+  // Mutual exclusion
+  std::mutex mutex;
+};
+
+}
+
+#endif
---
+++ test/measurement-trigger-test.cpp
@@ -0,0 +1,181 @@
+///////////////////////////////////////////////////////////////////////////\file
+///
+///   Copyright 2022 SINTEF AS
+///
+///   This Source Code Form is subject to the terms of the Mozilla
+///   Public License, v. 2.0. If a copy of the MPL was not distributed
+///   with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdexcept>
+#define BOOST_TEST_MODULE measurement_trigger_test
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+#include <chrono>
+#include <stdexcept>
+
+#include <i3ds/measurement_trigger.hpp>
+#include <i3ds/address_server.hpp>
+#include <i3ds/subscriber.hpp>
+#include <i3ds/emulated_imu.hpp>
+#include <i3ds/imu_client.hpp>
+
+using namespace i3ds;
+using namespace i3ds_asn1;
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct F
+{
+  F()
+    : node(20),
+      context(Context::Create()),
+      imu(EmulatedIMU::Create(context, node)),
+      imu_command_server(context),
+      client(context, node)
+  {
+    imu->Attach(imu_command_server);
+    imu_command_server.Start();
+    client.set_timeout(1000);
+
+    // Before we close, let other workers run, ::yield does not actually
+    // yield the cpu if the thread has more budget, so do a sleep that
+    // will force a sched_out
+    //std::this_thread::sleep_for(std::chrono::microseconds(10000));
+  }
+
+  ~F()
+  {
+    imu_command_server.Stop();
+  }
+
+  const NodeID node;
+  Context::Ptr context;
+  EmulatedIMU::Ptr imu;
+  Server imu_command_server;
+  IMUClient client;
+};
+
+BOOST_FIXTURE_TEST_SUITE(s, F)
+
+////////////////////////////////////////////////////////////////////////////////
+
+int received_count;
+
+void
+check_measurement(IMU::MeasurementTopic::Data& data)
+{
+  BOOST_CHECK_EQUAL(data.samples.nCount, 1);
+  BOOST_CHECK_EQUAL(data.samples.arr[0].axis_x_rate, 1.0);
+  BOOST_CHECK_EQUAL(data.samples.arr[0].axis_x_acceleration, 2.0);
+  BOOST_CHECK_EQUAL(data.samples.arr[0].axis_y_rate, 3.0);
+  BOOST_CHECK_EQUAL(data.samples.arr[0].axis_y_acceleration, 4.0);
+  BOOST_CHECK_EQUAL(data.samples.arr[0].axis_z_rate, 5.0);
+  BOOST_CHECK_EQUAL(data.samples.arr[0].axis_z_acceleration, 6.0);
+}
+
+void
+handle_measurement(IMU::MeasurementTopic::Data& data)
+{
+  BOOST_TEST_MESSAGE("Subscriber received msg at: " << data.attributes.timestamp);
+  check_measurement(data);
+  received_count++;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE(trigger_from_active)
+{
+  MeasurementTrigger<IMU::MeasurementTopic> trigger(context, node, true);
+  received_count = 0;
+
+  Subscriber subscriber(context);
+
+  subscriber.Attach<IMU::MeasurementTopic>(client.node(), &handle_measurement);
+  SamplePeriod period = 1000000;
+
+  client.set_state(StateCommand_activate);
+  client.set_sampling(period);
+  subscriber.Start();
+
+  trigger.trigger();
+
+  std::this_thread::sleep_for(std::chrono::microseconds(period * 2));
+
+  BOOST_CHECK_GE(received_count, 1);
+
+  check_measurement(trigger.fetch());
+  trigger.trigger();
+
+  std::this_thread::sleep_for(std::chrono::microseconds(period * 2));
+
+  BOOST_CHECK_GE(received_count, 2);
+  subscriber.Stop();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE(trigger_from_operational)
+{
+  MeasurementTrigger<IMU::MeasurementTopic> trigger(context, node, true);
+  received_count = 0;
+
+  Subscriber subscriber(context);
+
+  subscriber.Attach<IMU::MeasurementTopic>(client.node(), &handle_measurement);
+  SamplePeriod period = 1000000;
+
+  client.set_state(StateCommand_activate);
+  client.set_sampling(period);
+  client.set_state(StateCommand_start);
+  subscriber.Start();
+
+  trigger.trigger();
+
+  std::this_thread::sleep_for(std::chrono::microseconds(period * 2));
+
+  BOOST_CHECK_GE(received_count, 1);
+
+  check_measurement(trigger.fetch());
+  trigger.trigger();
+
+  std::this_thread::sleep_for(std::chrono::microseconds(period * 2));
+
+  BOOST_CHECK_GE(received_count, 2);
+  subscriber.Stop();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE(fetch_exception)
+{
+  MeasurementTrigger<IMU::MeasurementTopic> trigger(context, node);
+  received_count = 0;
+
+  Subscriber subscriber(context);
+
+  subscriber.Attach<IMU::MeasurementTopic>(client.node(), &handle_measurement);
+  SamplePeriod period = 1000000;
+
+  client.set_state(StateCommand_activate);
+  client.set_sampling(period);
+  subscriber.Start();
+
+  IMU::MeasurementTopic::Data data;
+  BOOST_CHECK_THROW(data = trigger.fetch(), std::logic_error);
+
+  trigger.trigger();
+
+  std::this_thread::sleep_for(std::chrono::microseconds(period * 2));
+
+  BOOST_CHECK_GE(received_count, 1);
+  BOOST_CHECK_THROW(data = trigger.fetch(), std::logic_error);
+  subscriber.Stop();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
